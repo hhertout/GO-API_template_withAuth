bacc85322b37927455b37b67fbc58514d4a72651
hhertout <hugues.hertout@gmail.com>
2023-02-20 09:39:07 +0100

    improve cors configuration

--- router/router.go
+++ router/router.go
@@ -1,20 +1,17 @@
 package router
 
 import (
+	"API_go/go_test/config"
 	"API_go/go_test/controllers"
 	"API_go/go_test/middleware"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Routes() {
 	router := gin.New()
 	router.SetTrustedProxies([]string{"*"})
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"*"}
-	router.Use(cors.New(config))
-	//router.Use(cors.Default())
+	router.Use(config.CORSConfig())
 
 	router.POST("/signup", controllers.Signup)
 	router.POST("/login", controllers.Login)
